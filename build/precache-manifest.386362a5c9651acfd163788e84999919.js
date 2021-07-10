6e1564d2be225458b71bc6c35356918634c54ffb
dineshdevlpr <dineshiaf30@gmail.com>
2021-07-10 07:14:45 +0530

    updated keeper app

---
+++ build/precache-manifest.386362a5c9651acfd163788e84999919.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58663cc1adc0acce568d848500936e4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "19c87e705023a76364bc",
+    "url": "/static/js/main.f430aa12.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
